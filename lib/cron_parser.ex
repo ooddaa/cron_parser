d82de39a9730286e97e34d596748f753c976381f
oda <ooddaa@gmail.com>
2023-12-04 22:13:37 +0000

    parser works, tests pass
    TODO:
    - add `pretty_print/1`
    - refactor

--- lib/cron_parser.ex
+++ lib/cron_parser.ex
@@ -5,7 +5,132 @@ defmodule CronParser do
 
   @doc """
   """
-    def parser(input) do
-      nil
+  def parse_to_ast(input) when is_binary(input) do
+    case validate_input(input) do
+      true ->
+        with {:ok, minutes, rest} <- parse_minutes(input),
+             {:ok, hours, rest} <- parse_hours(rest),
+             {:ok, days, rest} <- parse_days(rest),
+             {:ok, months, rest} <- parse_months(rest),
+             {:ok, weekdays, rest} <- parse_weekdays(rest),
+             {:ok, cmd, nil} <- parse_cmd(rest) do
+          %{
+            "minutes" => minutes,
+            "hours" => hours,
+            "days" => days,
+            "months" => months,
+            "weekdays" => weekdays,
+            "cmd" => cmd
+          }
+        end
+
+      false ->
+        {:error, :bad_input, input}
     end
   end
+
+  def parse_to_ast(input), do: {:error, :bad_input, input}
+
+  def parse_minutes(input) do
+    [input, rest] = String.split(input, " ", parts: 2)
+    minutes = process(input, &minute/1)
+    {:ok, minutes, rest}
+  end
+
+  defp minute("*"), do: {0, 59, 1}
+  defp minute(input), do: parse(input, &minute/1)
+
+  def parse_hours(input) do
+    [input, rest] = String.split(input, " ", parts: 2)
+    hours = process(input, &hour/1)
+    {:ok, hours, rest}
+  end
+
+  defp hour("*"), do: {0, 23, 1}
+  defp hour(input), do: parse(input, &hour/1)
+
+  def parse_days(input) do
+    [input, rest] = String.split(input, " ", parts: 2)
+    days = process(input, &day/1)
+    {:ok, days, rest}
+  end
+
+  defp day("*"), do: {1, 31, 1}
+  defp day(input), do: parse(input, &day/1)
+
+  def parse_months(input) do
+    [input, rest] = String.split(input, " ", parts: 2)
+    months = process(input, &month/1)
+    {:ok, months, rest}
+  end
+
+  defp month("*"), do: {1, 12, 1}
+  defp month(input), do: parse(input, &month/1)
+
+  def parse_weekdays(input) do
+    [input, rest] = String.split(input, " ", parts: 2)
+    weekdays = process(input, &weekday/1)
+    {:ok, weekdays, rest}
+  end
+
+  defp weekday("*"), do: {0, 6, 1}
+  defp weekday(input), do: parse(input, &weekday/1)
+
+  def parse_cmd(input) do
+    cmds = process(input, &cmd/1)
+    {:ok, cmds, nil}
+  end
+
+  defp cmd(input), do: input
+
+  defp validate_input(input) do
+    input
+    |> String.split(" ", trim: true)
+    |> then(&(length(&1) == 6))
+  end
+
+  defp process(input, parser) do
+    input
+    |> fragment_input()
+    |> Enum.map(&parser.(&1))
+    |> normalize()
+  end
+
+  defp fragment_input(input) do
+    input
+    |> String.split(",", trim: true)
+  end
+
+  defp parse(input, parser) do
+    cond do
+      String.contains?(input, "-") ->
+        [from, to] = String.split(input, "-")
+        {String.to_integer(from), String.to_integer(to), 1}
+
+      String.contains?(input, "/") ->
+        [range, step] = String.split(input, "/")
+        {from, to, _} = parser.(range)
+        {from, to, String.to_integer(step)}
+
+      true ->
+        {String.to_integer(input), String.to_integer(input), 1}
+    end
+  end
+
+  defp normalize(lists) do
+    lists
+    |> Enum.map(&expand_range/1)
+    |> Enum.flat_map(& &1)
+    |> Enum.uniq()
+    |> Enum.sort()
+  end
+
+  defp expand_range({from, to, step}) do
+    Range.to_list(from..to//step)
+  end
+  defp expand_range(input), do: [input]
+
+  def pretty_print(input) do
+    input
+  end
+end
--- test/cron_parser_test.exs
+++ test/cron_parser_test.exs
@@ -1,7 +1,148 @@
 defmodule CronParserTest do
   use ExUnit.Case
 
-  test "parser" do
-    assert true == true
+  # describe "final" do
+  #   test "happy case" do
+  #     input = "*/15 0 1,15 * 1-5 /usr/bin/find"
+  #
+  #     result = """
+  #     minute        0 15 30 45
+  #     hour          0
+  #     day of month  1 15
+  #     month         1 2 3 4 5 6 7 8 9 10 11 12
+  #     day of week   1 2 3 4 5
+  #     command       /usr/bin/find
+  #     """
+  #   end
+  # end
+
+  describe "parsing to ast" do
+    # test "first" do 
+    #   
+    #   input = "* * * * * /usr/bin/find"
+    #   CronParser.parse_to_ast(input)
+    #   |> IO.inspect()
+    #  end
+
+    test "max" do
+      input = "* * * * * /usr/bin/find"
+
+      ast = %{
+        "minutes" => Enum.map(0..59, &(&1)),
+        "hours" => Enum.map(0..23, &(&1)),
+        "days" => Enum.map(1..31, &(&1)),
+        "months" => Enum.map(1..12, &(&1)),
+        "weekdays" => Enum.map(0..6, &(&1)),
+        "cmd" => ["/usr/bin/find"]
+      }
+
+      result = CronParser.parse_to_ast(input)
+      assert result["minutes"] == ast["minutes"]
+      assert result == ast
+    end
+
+    test "steps" do
+      input = "*/15 */6 */5 */3 */2 /usr/bin/find"
+
+      ast = %{
+        "minutes" => [0, 15, 30, 45],
+        "hours" => [0, 6, 12, 18],
+        "days" => [1, 6, 11, 16, 21, 26, 31], 
+        "months" => [1, 4, 7, 10],  
+        "weekdays" => [0, 2, 4, 6],  
+        "cmd" => ["/usr/bin/find"]
+      }
+      result = CronParser.parse_to_ast(input)
+      assert result == ast
+    end
+   
+    test "ranges happy" do
+      input = "0-16 12-16 1-10 6-12 0-6 /usr/bin/find"
+
+      ast = %{
+        "minutes" => Enum.map(0..16, &(&1)),
+        "hours" => Enum.map(12..16, &(&1)),
+        "days" => Enum.map(1..10, &(&1)),
+        "months" => Enum.map(6..12, &(&1)),
+        "weekdays" => [0, 1, 2, 3, 4, 5, 6],  
+        "cmd" => ["/usr/bin/find"]
+      }
+      result = CronParser.parse_to_ast(input)
+      assert result == ast
+    end
+    
+    test "multiple ranges happy" do
+      input = "0-8,9-16 12-14,15-16 1-5,6-10 6-9,10-12 0-1,4-6 /usr/bin/find"
+
+      ast = %{
+        "minutes" => Enum.map(0..16, &(&1)),
+        "hours" => Enum.map(12..16, &(&1)),
+        "days" => Enum.map(1..10, &(&1)),
+        "months" => Enum.map(6..12, &(&1)),
+        "weekdays" => [0, 1, 4, 5, 6],  
+        "cmd" => ["/usr/bin/find"]
+      }
+
+      result = CronParser.parse_to_ast(input)
+      assert result == ast
+    end
+    
+    test "selection happy" do
+      input = "8,9 14,15 5,6 9,10 1,4 /usr/bin/find"
+
+      ast = %{
+        "minutes" => [8,9],
+        "hours" => [14,15],
+        "days" => [5,6],
+        "months" => [9,10],
+        "weekdays" => [1, 4],  
+        "cmd" => ["/usr/bin/find"]
+      }
+
+      result = CronParser.parse_to_ast(input)
+      assert result == ast
+    end
+    
+    test "selection one value" do
+      input = "0 1 2 3 4 /usr/bin/find"
+
+      ast = %{
+        "minutes" => [0],
+        "hours" => [1],
+        "days" => [2],
+        "months" => [3],
+        "weekdays" => [4],  
+        "cmd" => ["/usr/bin/find"]
+      }
+
+      result = CronParser.parse_to_ast(input)
+      assert result == ast
+    end
+
+    test "final happy case" do
+      input = "*/15 0 1,15 * 1-5 /usr/bin/find"
+
+      ast = %{
+        "minutes" => [0, 15, 30, 45],
+        "hours" => [0],
+        "days" => [1,15],
+        "months" => Enum.map(1..12, &(&1)),
+        "weekdays" => Enum.map(1..5, &(&1)),
+        "cmd" => ["/usr/bin/find"]
+      }
+
+      result = CronParser.parse_to_ast(input)
+      assert result == ast
+    end
+  end
+
+  test "pretty print" do
+    input = "*/15 0 1,15 * 1-5 /usr/bin/find"
+
+    ast = CronParser.parse_to_ast(input)
+    CronParser.pretty_print(ast)
+    # |> IO.inspect(binaries: :as_strings)
   end
 end
+
+
